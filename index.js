bb5589e196dc439045f4f0b1aeeafd9715cd6394
bhuwan1998 <rfperfect785@gmail.com>
2020-12-24 16:14:24 +1100

    Callback function

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ var rect = require('./rectangle');
 
 function solveRect(l, b) {
     console.log("Solving for rectange with l = " + l + " and b = " + b ); 
+    console.log("");
+    
+    rect(l, b, (err, rectangle) => {
+        if(err){
+            console.log("Error: ", err.message);
+            console.log("");
+        }
+        else{
+            console.log("The area of rectangle of dimensions l = " + l + " and b = " + b +  " is " + rectangle.area());
+            console.log("");
+            console.log("The perimeter of rectangle of dimensions l = " + l + " and b = " + b +  " is " + rectangle.perimeter());
 
-    if( l <= 0 || b <= 0){
-        console.log("Rectangle dimensions are supposed to be greater than zero: l = " + l + ", and b = " + b );
-        console.log("")
-    }
-    else{
-        console.log("The area of the rectangle is " + rect.area(l,b));
-        console.log("")
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-
-    }
+        }
+    });
+    console.log("this statement is after the call to rect()");          
 }
 
 solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
 solveRect(-3,5);
+
--- rectangle.js
+++ rectangle.js
@@ -1,4 +1,22 @@
-exports.perimeter = (x,y) => (2*(x+y))
-exports.area = (x,y) => (x*y)
+module.exports = (x,y, callback) => {
+    if(x <= 0 || y <= 0){
+        setTimeout(() => 
+        callback(new Error("Rectangle dimensions are supposed to be greater than zero: l = " + x + ", and b = " + y ),
+            null), // first parameter will pass in an error and second will be null 
+        2000);
+    }
+    else{
+        setTimeout(() => 
+        callback( null,
+            {
+                perimeter: () => (2*(x+y)),
+                area: () => x*y
+                
+            }), // first parameter will pass in an error and second will be null 
+        2000);
+
+    }
+}
+
 
 
